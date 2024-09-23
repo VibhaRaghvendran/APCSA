9f75aaf811c715b4233e8e821b33fc70535771cd
VibhaRaghvendran <vibha.shyama@gmail.com>
2024-09-23 14:59:42 -0700

    Update MVCipher.java

--- MVCipher.java
+++ MVCipher.java
@@ -10,37 +10,37 @@ import java.util.Scanner;
 public class MVCipher {
 	
 	String keyword = "";
+	Scanner scan = new Scanner (System.in);
+	boolean found;
 		
 	/** Constructor */
 	public MVCipher() { 
 		
 	}
-	
-	public static void main(String[] args) {
-		MVCipher mvc = new MVCipher();
-		mvc.run();
-	}
-	
+
 	/**
 	 *	Method header goes here
 	 */
 	public void run() {
-		System.out.println("\n Welcome to the MV Cipher machine!\n");
 		Scanner scan = new Scanner (System.in);
-		boolean found = false;
+		found = false;
 		
 		System.out.println("Welcome to the MV Cipher machine!");
 		System.out.println();
 		System.out.println("Please input a word to use as key (letters only):");
 		keyword = scan.nextLine();
 		
+		// update checking
 		while (!found) {
-			
-		}
-		
-		
-		/* Prompt for encrypt or decrypt */
-			
+			if (keyword.length() < 3) {
+				System.out.println("ERROR: Key must be all letters and at least 3 characters long");
+			}
+			/*
+			for (int i = 0; i < keyword.length(); i++) {
+				if (keyword.charAt(i)
+			}
+			*/
+		}	
 			
 		/* Prompt for an input file name */
 		
@@ -55,5 +55,71 @@ public class MVCipher {
 	}
 	
 	// other methods go here
+	public String mode () {
+		String answer;
+		
+		/* Prompt for encrypt or decrypt */
+		System.out.println("Encrypt(e) or decrypt(d)?"); 	
+		answer = scan.nextLine();
+		found = false;
+		 
+		while (!found) {
+			if (answer.equalsIgnoreCase("e")) {
+				found = true;
+			}		
+			else if (answer.equalsIgnoreCase("d")) {
+				found = true;
+			}
+			else {
+				found = false;
+				System.out.println("Not valid. Try again");
+				answer = scan.nextLine();
+			}
+		}
+		
+		return answer;
+	}
+	
+	public String inputOutput () {
+		String answer;
+		
+		/* Prompt for encrypt or decrypt */
+		System.out.println("Encrypt(e) or decrypt(d)?"); 	
+		answer = scan.nextLine();
+		found = false;
+		 
+		while (!found) {
+			if (answer.equalsIgnoreCase("e")) {
+				found = true;
+			}		
+			else if (answer.equalsIgnoreCase("d")) {
+				found = true;
+			}
+			else {
+				found = false;
+				System.out.println("Not valid. Try again");
+				answer = scan.nextLine();
+			}
+		}
+		
+		return answer;
+	}
+	
+	public void encrypt () {
+		System.out.println("encrypting");
+	}
+	
+	public String encryptOneWord (String a) {
+		for (int i = 0; i < 
+	}
+	
+	public void decrypt () {
+		System.out.println("encrypting");
+	}
+	
+	public static void main(String[] args) {
+		MVCipher mvc = new MVCipher();
+		mvc.run();
+	}
 	
 }
