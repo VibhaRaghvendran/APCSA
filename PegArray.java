fa815a8fd5f90cbd3b261177b2421a5317d98acd
VibhaRaghvendran <vibha.shyama@gmail.com>
2024-10-07 14:30:24 -0700

    Update PegArray.java

--- PegArray.java
+++ PegArray.java
@@ -1,98 +1,160 @@
 /**
- *  This class creates and manages one array of pegs from the game MasterMind.
- *
- *  @author
- *  @since
+ *	Plays the game of MasterMind.
+ *	<Describe the game here>
+ *	@author
+ *	@since
  */
 
-public class PegArray {
+import java.util.Scanner;
 
-	// array of pegs
-	private Peg [] pegs;
+public class MasterMind {
 
-	// the number of exact and partial matches for this array
-	// as matched against the master.
-	// Precondition: these values are valid after getExactMatches() and getPartialMatches()
-	//				are called
-	private int exactMatches, partialMatches;
+	private boolean reveal;			// true = reveal the master combination
+	private PegArray[] guesses;		// the array of guessed peg arrays
+	private PegArray master;		// the master (key) peg array
 
-	/**
-	 *	Constructor
-	 *	@param numPegs	number of pegs in the array
-	 */
-	public PegArray(int numPegs) {
+	// Constants
+	private final int PEGS_IN_CODE = 4;		// Number of pegs in code
+	private final int MAX_GUESSES = 10;		// Max number of guesses
+	private final int PEG_LETTERS = 6;		// Number of different letters on pegs
+	// 6 = A through F
+	private int turns = 0;
 
-	}
-
-	/**
-	 *	Return the peg object
-	 *	@param n	The peg index into the array
-	 *	@return		the peg object
-	 */
-	public Peg getPeg(int n) { 
-
-		return pegs[n]; 
-	}
-
-	/**
-	 *  Finds exact matches between master (key) peg array and this peg array
-	 *	Postcondition: field exactMatches contains the matches with the master
-	 *  @param master	The master (code) peg array
-	 *	@return			The number of exact matches
-	 */
-	public int getExactMatches(PegArray master) { 
-		for (int i = 0; i < pegs.length; i++) {
-			if (pegs[i].getLetter() == master.getPeg(i).getLetter()) { // checks if the letters at the same index are the same
-				exactMatches++;
-			}
+	public MasterMind () {
+		printIntroduction();
+		while (turns < 10) {
+			System.out.println("Guess " + (turns + 1) + ":");
+			System.out.println();
+			askingGuess();
+			printBoard();
+			printGuess(turns);
+			turns++;
 		}
-
-		return exactMatches;
 	}
-	
-	/**
-	 *  Find partial matches between master (key) peg array and this peg array
-	 *	Postcondition: field partialMatches contains the matches with the master
-	 *  @param master	The master (code) peg array
-	 *	@return			The number of partial matches
-	 */
-	
-	public int getPartialMatches(PegArray master) { 
-		for (int i = 0; i < pegs.length; i++) {
-			for (int j = 0; j < pegs.length; j++) {
-				int indexI = 0;
-				int indexJ = 0;
-				if (master.getPeg(i).getLetter() == pegs[j].getLetter() && i != j) {
-					partialMatches++;
-					indexI = i;
-					indexJ = j;
-					if (master.getPeg(indexI).getLetter() == pegs[indexI].getLetter()) {
-						partialMatches--;
+
+	public String askingGuess () {
+		String guess = "";
+		boolean found = false;
+		Scanner scan = new Scanner (System.in);
+
+		System.out.println("What is your guess?");
+		guess = scan.nextLine();
+
+		while (!found) {
+			int count = 0;
+			if (guess.length() == 4) {
+				found = true;
+
+				for (int i = 0; i < guess.length(); i++) {
+					if (guess.charAt(i) >= 65 && guess.charAt(i) <= 70) {
+						count++;
 					}
-					else if (master.getPeg(indexJ).getLetter() == pegs[indexJ].getLetter()) {
-						partialMatches--;
-					}					
-					else {
-						pegs[indexJ].setLetter('G');
-						break;
+					else if (guess.charAt(i) >= 97 && guess.charAt(i) <= 102) {
+						count++;
 					}
 				}
-					
+
+				if (count == guess.length()) {
+					found = true;
+				}
+			}
+
+			else {
+				System.out.println("Invalid input. Try again:");
+				guess = scan.nextLine();
 			}
 		}
 
-		return partialMatches;
+		String finalGuess = guess.toUpperCase();
+		return finalGuess;
 	}
 
-	// Accessor methods
-	// Precondition: getExactMatches() and getPartialMatches() must be called first
-	public int getExact() { 
-		return exactMatches; 
-
+	/**
+	 *	Print the introduction screen
+	 */
+	public void printIntroduction() {
+		System.out.println("\n");
+		System.out.println("+------------------------------------------------------------------------------------+");
+		System.out.println("| ___  ___             _              ___  ___ _             _                       |");
+		System.out.println("| |  \\/  |            | |             |  \\/  |(_)           | |                      |");
+		System.out.println("| | .  . |  __ _  ___ | |_  ___  _ __ | .  . | _  _ __    __| |                      |");
+		System.out.println("| | |\\/| | / _` |/ __|| __|/ _ \\| '__|| |\\/| || || '_ \\  / _` |                      |");
+		System.out.println("| | |  | || (_| |\\__ \\| |_|  __/| |   | |  | || || | | || (_| |                      |");
+		System.out.println("| \\_|  |_/ \\__,_||___/ \\__|\\___||_|   \\_|  |_/|_||_| |_| \\__,_|                      |");
+		System.out.println("|                                                                                    |");
+		System.out.println("| WELCOME TO MONTA VISTA MASTERMIND!                                                 |");
+		System.out.println("|                                                                                    |");
+		System.out.println("| The game of MasterMind is played on a four-peg gameboard, and six peg letters can  |");
+		System.out.println("| be used.  First, the computer will choose a random combination of four pegs, using |");
+		System.out.println("| some of the six letters (A, B, C, D, E, and F).  Repeats are allowed, so there are |");
+		System.out.println("| 6 * 6 * 6 * 6 = 1296 possible combinations.  This \"master code\" is then hidden     |");
+		System.out.println("| from the player, and the player starts making guesses at the master code.  The     |");
+		System.out.println("| player has 10 turns to guess the code.  Each time the player makes a guess for     |");
+		System.out.println("| the 4-peg code, the number of exact matches and partial matches are then reported  |");
+		System.out.println("| back to the user. If the player finds the exact code, the game ends with a win.    |");
+		System.out.println("| If the player does not find the master code after 10 guesses, the game ends with   |");
+		System.out.println("| a loss.                                                                            |");
+		System.out.println("|                                                                                    |");
+		System.out.println("| LET'S PLAY SOME MASTERMIND!                                                        |");
+		System.out.println("+------------------------------------------------------------------------------------+");
+		System.out.println("\n");
 	}
-	public int getPartial() { 
-		return partialMatches; 
 
+	/**
+	 *	Print the peg board to the screen
+	 */
+	public void printBoard() {
+		// Print header
+		System.out.print("+--------+");
+		for (int a = 0; a < PEGS_IN_CODE; a++) System.out.print("-------+");
+		System.out.println("---------------+");
+		System.out.print("| MASTER |");
+		for (int a = 0; a < PEGS_IN_CODE; a++)
+			if (reveal)
+				System.out.printf("   %c   |", master.getPeg(a).getLetter());
+			else
+				System.out.print("  ***  |");
+		System.out.println(" Exact Partial |");
+		System.out.print("|        +");
+		for (int a = 0; a < PEGS_IN_CODE; a++) System.out.print("-------+");
+		System.out.println("               |");
+		// Print Guesses
+		System.out.print("| GUESS  +" );
+		for (int a = 0; a < PEGS_IN_CODE; a++) System.out.print("-------+");
+		System.out.println("---------------|");
+		for (int g = 0; g < MAX_GUESSES - 1; g++) {
+			printGuess(g);
+			System.out.println("|        +-------+-------+-------+-------+---------------|");
+		}
+		printGuess(MAX_GUESSES - 1);
+		// print bottom
+		System.out.print("+--------+");
+		for (int a = 0; a < PEGS_IN_CODE; a++) System.out.print("-------+");
+		System.out.println("---------------+");
+	}
+
+	/**
+	 *	Print one guess line to screen
+	 *	@param t	the guess turn
+	 */
+	public void printGuess(int t) {
+		
+		System.out.printf("|   %2d   |", (t + 1));
+		// If peg letter in the A to F range
+		char c = guesses[t].getPeg(0).getLetter();
+		if (c >= 'A' && c <= 'F')
+			for (int p = 0; p < PEGS_IN_CODE; p++)
+				System.out.print("   " + guesses[t].getPeg(p).getLetter() + "   |");
+		// If peg letters are not A to F range
+		else
+			for (int p = 0; p < PEGS_IN_CODE; p++)
+				System.out.print("       |");
+		System.out.printf("   %d      %d    |\n",
+				guesses[t].getExact(), guesses[t].getPartial());
+	}
+
+	public static void main (String[] args) {
+		MasterMind master = new MasterMind();
 	}
 
 }
