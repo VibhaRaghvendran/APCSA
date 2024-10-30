ef212d0770b6ab79047d73cb0ec9c6fc396974ee
VibhaRaghvendran <vibha.shyama@gmail.com>
2024-10-30 14:21:17 -0700

    Create Yahtzee.java

---
+++ Yahtzee.java
@@ -0,0 +1,48 @@
+import java.util.Scanner;
+
+public class Yahtzee {
+	
+	private String player1, player2;
+	Scanner scan;
+	
+	public static void main (String[] args) {
+		Yahtzee game = new Yahtzee();
+	}
+	
+	public Yahtzee () {
+		scan = new Scanner (System.in);
+		printHeader();
+		playerNames();
+		DiceGroup diceGroup = new DiceGroup();
+		diceGroup.printDice();
+	}
+	
+	public void playerNames () {
+		System.out.println("Player 1, please enter your first name:");
+		player1 = scan.nextLine();
+		System.out.println("Player 2, please enter your first name:");
+		player2 = scan.nextLine();
+	}
+	
+	public void printHeader() {
+		System.out.println("\n");
+		System.out.println("+------------------------------------------------------------------------------------+");
+		System.out.println("| WELCOME TO MONTA VISTA YAHTZEE!                                                    |");
+		System.out.println("|                                                                                    |");
+		System.out.println("| There are 13 rounds in a game of Yahtzee. In each turn, a player can roll his/her  |");
+		System.out.println("| dice up to 3 times in order to get the desired combination. On the first roll, the |");
+		System.out.println("| player rolls all five of the dice at once. On the second and third rolls, the      |");
+		System.out.println("| player can roll any number of dice he/she wants to, including none or all of them, |");
+		System.out.println("| trying to get a good combination.                                                  |");
+		System.out.println("| The player can choose whether he/she wants to roll once, twice or three times in   |");
+		System.out.println("| each turn. After the three rolls in a turn, the player must put his/her score down |");
+		System.out.println("| on the scorecard, under any one of the thirteen categories. The score that the     |");
+		System.out.println("| player finally gets for that turn depends on the category/box that he/she chooses  |");
+		System.out.println("| and the combination that he/she got by rolling the dice. But once a box is chosen  |");
+		System.out.println("| on the score card, it can't be chosen again.                                       |");
+		System.out.println("|                                                                                    |");
+		System.out.println("| LET'S PLAY SOME YAHTZEE!                                                           |");
+		System.out.println("+------------------------------------------------------------------------------------+");
+		System.out.println("\n\n");
+	}
+}
