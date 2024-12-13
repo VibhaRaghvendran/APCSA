9dabdf5e7825dc5cb90eb9598fc6cacfac940f2d
VibhaRaghvendran <vibha.shyama@gmail.com>
2024-12-12 22:41:55 -0800

    Update SortMethods.java

--- SortMethods.java
+++ SortMethods.java
@@ -1,3 +1,7 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 /**
  *	SortMethods - Sorts an array of Integers in ascending order.
  *
@@ -5,15 +9,17 @@
  *	@since November 26 2024
  */
 public class SortMethods {
+    private City[] temp;
+    private CityComparatorByName compare = new CityComparatorByName();
 
     /**
-     *	Bubble Sort algorithm in ascending order
-     *	@param arr		array of Integer objects to sort
+     *	Bubble Sort algorithm in descending order
+     *	@param arr		ArrayList of City objects to sort
      */
-    private void bubbleSort (Integer [] arr) {
-        for (int i = 0; i < arr.length; i++) { // goes through array n times (until sorted)
-            for (int j = 1; j < (arr.length - i); j++) { //goes through the unsorted portion of the array
-                if (arr[j - 1] > arr[j]) { // if greater, swap
+    public void bubbleSort (List<City> arr) {
+        for (int i = 0; i < arr.size(); i++) { // goes through array n times (until sorted)
+            for (int j = 1; j < (arr.size() - i); j++) { //goes through the unsorted portion of the array
+                if (arr.get(j - 1).compareTo(arr.get(j)) < 0) { // if greater, swap
                     arr = swap(arr,j - 1, j);
                 }
             }
@@ -21,28 +27,28 @@ public class SortMethods {
     }
 
     /**
-     *	Swaps two Integer objects in array arr
-     *	@param arr		array of Integer objects
+     *	Swaps two City objects in ArrayList arr
+     *	@param arr		ArrayList of City objects
      *	@param x		index of first object to swap
      *	@param y		index of second object to swap
      */
-    private Integer[] swap (Integer[] arr, int x, int y) {
-        int temp = arr[x]; // stores first value in a variable
-        arr[x] = arr[y]; // transfers second value to first index
-        arr[y] = temp; // transfers the stored value to the second index
+    private List<City> swap (List<City> arr, int x, int y) {
+        City temp = arr.get(x); // stores first value in a variable
+        arr.set(x, arr.get(y)); // transfers second value to first index
+        arr.set(y, temp); // transfers the stored value to the second index
 
         return arr;
     }
 
     /**
      *	Selection Sort algorithm in ascending order
-     *	@param arr		array of Integer objects to sort
+     *	@param arr		ArrayList of City objects to sort
      */
-    public void selectionSort(Integer [] arr) {
-        for (int i = arr.length; i > 1; i--) { // goes through the array n times (until sorted)
+    public void selectionSort(List<City> arr) {
+        for (int i = arr.size(); i > 1; i--) { // goes through the array n times (until sorted)
             int max = 0;
             for (int j = 1; j < i; j++) { // finds the maximum element
-                if (arr[j] > arr[max]) {
+                if (arr.get(j).compareTo(arr.get(max)) > 0) {
                     max = j; // sets the index of the maximum element
                 }
             }
@@ -53,80 +59,184 @@ public class SortMethods {
 
     /**
      *	Insertion Sort algorithm in ascending order
-     *	@param arr		array of Integer objects to sort
+     *	@param arr		ArrayList of City objects to sort
      */
-    public void insertionSort(Integer [] arr) {
-        for (int i = 1; i < arr.length; i++) {
-            int temp = arr[i]; // stores the temporary value
+    public void insertionSort(List<City> arr) {
+        for (int i = 1; i < arr.size(); i++) {
+            City temp = arr.get(i); // stores the temporary value
 
             int a = i; // only sorts the subarray (index set by i)
-            while (a > 0 && temp < arr[a - 1]) { // sorts the sub array (comparing elements until it is in ascending order)
-                arr[a] = arr[a - 1];
+            while (a > 0 && compare.compare(temp, arr.get(a - 1)) < 0) { // sorts the sub array (comparing elements until it is in ascending order)
+                arr.set(a, arr.get(a - 1));
                 a--;
             }
 
-            arr[a] = temp; // transfers stored value
+            arr.set(a, temp); // transfers stored value
         }
     }
 
     /**
-     *	Merge Sort algorithm in ascending order
-     *	@param arr		array of Integer objects to sort
+     *	Merge Sort algorithm in descending order for population
+     *	@param a		ArrayList of City objects to sort
+     *  @return a       ArrayList of sorted City objects
      */
-    public void mergeSort(Integer [] arr) {
-        arr = recursiveSort(arr,0, arr.length - 1);
-    }
+    public List<City> mergeSort1(List<City> a) {
+        int n = a.size();
+        City[] yay = a.toArray(new City[0]);
+        temp = new City[n];
+        recursiveSort1(yay,0, n - 1);
 
-    private Integer[] recursiveSort (Integer[] a, int from, int to) {
-        if (to - from < 2) { // checks if the array's length is 2 or less
-            if (to > from && a[to] < a[from]) { // if the array elements are out of order, put them in order
-                a = swap (a, to, from);
-            }
-            else {
-                int middle = (from + to)/2; // splits the array in two more components
-                // recursion keeps going until all arrays are 2 elements or less
-                recursiveSort(a, from, middle);
-                recursiveSort(a,middle + 1, to);
-                // merges the arrays
-                a = merge (a, from, middle, to);
-            }
-        }
-
+        a = new ArrayList<>((Arrays.asList(yay)));
         return a;
     }
 
-    private Integer[] merge (Integer[] a, int from, int middle, int to) {
-        Integer[] temp = new Integer[a.length - 1];
-        int i = from, j = middle + 1, k = from;
+    /**
+     * Helper method for Merge Sort (population) - splits
+     * @param a             Array of City objects
+     * @param from          starting index
+     * @param to            ending index
+     */
+    private void recursiveSort1 (City[] a, int from, int to) {
+        if (to - from < 2) { // base case: 1 or 2 elements
+            if (to > from && a[to].compareTo(a[from]) > 0) {
+                City aTemp = a[to];
+                a[to] = a[from];
+                a[from] = aTemp;
+            }
+        }
+        else { // recursive case
+            int middle = (from + to)/2;
+            recursiveSort1(a, from, middle);
+            recursiveSort1(a, middle+1, to);
+            //System.out.println("from: " + from + " middle: " + middle + " to: " + to);
+            merge1(a, from, middle, to);
+        }
+    }
 
+    /**
+     * Helper method for Merge Sort (population) - merges
+     * @param a             Array of City objects
+     * @param from          starting index
+     * @param middle        middle index (where the two arrays merge)
+     * @param to            end index
+     */
+    private void merge1 (City[] a, int from, int middle, int to) {
+        int i = from, j = middle + 1, k = from;
+        //System.out.println("i: " + i + " j: " + j + " k: " + k);
+
+        // while both arrays have elements left unprocessed:
         while (i <= middle && j <= to) {
-            if (a[i] < a[j]) {
+            if (a[i].compareTo(a[j]) > 0) {
                 temp[k] = a[i];
                 i++;
             }
             else {
-                temp[k] = a[j];
+                temp[k] = a[j];;
                 j++;
             }
             k++;
         }
 
+        // copy tail of the first half into temp
         while (i <= middle) {
             temp[k] = a[i];
             i++;
             k++;
         }
+
+        // copy tail of the second half into temp
         while (j <= to) {
             temp[k] = a[j];
             j++;
             k++;
         }
 
+        // copy back into original array
         for (k = from; k <= to; k++) {
             a[k] = temp[k];
         }
+    }
 
+    /**
+     *	Merge Sort algorithm in descending order for city names
+     *	@param a		ArrayList of City objects to sort
+     *  @return a       ArrayList of sorted City objects
+     */
+    public List<City> mergeSort2(List<City> a) {
+        int n = a.size();
+        City[] yay = a.toArray(new City[0]);
+        temp = new City[n];
+        recursiveSort2(yay,0, n - 1);
+
+        a = new ArrayList<>((Arrays.asList(yay)));
         return a;
+    }
+
+    /**
+     * Helper method for Merge Sort (city names) - splits
+     * @param a             Array of City objects
+     * @param from          starting index
+     * @param to            ending index
+     */
+    private void recursiveSort2 (City[] a, int from, int to) {
+        if (to - from < 2) { // base case: 1 or 2 elements
+            if (to > from && compare.compare(a[to], a[from]) > 0) {
+                City aTemp = a[to];
+                a[to] = a[from];
+                a[from] = aTemp;
+            }
+        }
+        else { // recursive case
+            int middle = (from + to)/2;
+            recursiveSort2(a, from, middle);
+            recursiveSort2(a, middle+1, to);
+            //System.out.println("from: " + from + " middle: " + middle + " to: " + to);
+            merge2(a, from, middle, to);
+        }
+    }
+
+    /**
+     * Helper method for Merge Sort (city names) - merges
+     * @param a             Array of City objects
+     * @param from          starting index
+     * @param middle        middle index (where the two arrays merge)
+     * @param to            end index
+     */
+    private void merge2 (City[] a, int from, int middle, int to) {
+        int i = from, j = middle + 1, k = from;
+        //System.out.println("i: " + i + " j: " + j + " k: " + k);
+
+        // while both arrays have elements left unprocessed:
+        while (i <= middle && j <= to) {
+            if (compare.compare(a[i], a[j]) > 0) {
+                temp[k] = a[i];
+                i++;
+            }
+            else {
+                temp[k] = a[j];;
+                j++;
+            }
+            k++;
+        }
+
+        // copy tail of the first half into temp
+        while (i <= middle) {
+            temp[k] = a[i];
+            i++;
+            k++;
+        }
+
+        // copy tail of the second half into temp
+        while (j <= to) {
+            temp[k] = a[j];
+            j++;
+            k++;
+        }
+
+        // copy back into original array
+        for (k = from; k <= to; k++) {
+            a[k] = temp[k];
+        }
     }
 
 /*****************************************************************/
@@ -149,7 +259,7 @@ public class SortMethods {
 
     public static void main(String[] args) {
         SortMethods se = new SortMethods();
-        se.run();
+        //se.run();
     }
 
     public void run() {
@@ -161,47 +271,45 @@ public class SortMethods {
         System.out.println("Array before sort:");
         printArray(arr);
         System.out.println();
-        bubbleSort(arr);
+        //bubbleSort(arr);
         System.out.println("Array after sort:");
         printArray(arr);
         System.out.println();
 
-		for (int a = 0; a < 10; a++)
-			arr[a] = (int)(Math.random() * 100) + 1;
-		System.out.println("\nSelection Sort");
-		System.out.println("Array before sort:");
-		printArray(arr);
-		System.out.println();
-		selectionSort(arr);
-		System.out.println("Array after sort:");
-		printArray(arr);
-		System.out.println();
+        for (int a = 0; a < 10; a++)
+            arr[a] = (int)(Math.random() * 100) + 1;
+        System.out.println("\nSelection Sort");
+        System.out.println("Array before sort:");
+        printArray(arr);
+        System.out.println();
+        //selectionSort(arr);
+        System.out.println("Array after sort:");
+        printArray(arr);
+        System.out.println();
 
-		for (int a = 0; a < 10; a++)
-			arr[a] = (int)(Math.random() * 100) + 1;
-		System.out.println("\nInsertion Sort");
-		System.out.println("Array before sort:");
-		printArray(arr);
-		System.out.println();
-		insertionSort(arr);
-		System.out.println("Array after sort:");
-		printArray(arr);
-		System.out.println();
+        for (int a = 0; a < 10; a++)
+            arr[a] = (int)(Math.random() * 100) + 1;
+        System.out.println("\nInsertion Sort");
+        System.out.println("Array before sort:");
+        printArray(arr);
+        System.out.println();
+        //insertionSort(arr);
+        System.out.println("Array after sort:");
+        printArray(arr);
+        System.out.println();
 //
 //
-		for (int a = 0; a < 10; a++)
-			arr[a] = (int)(Math.random() * 100) + 1;
-		System.out.println("\nMerge Sort");
-		System.out.println("Array before sort:");
-		printArray(arr);
-		System.out.println();
-		mergeSort(arr);
-		System.out.println("Array after sort:");
-		printArray(arr);
-		System.out.println();
+        for (int a = 0; a < 10; a++)
+            arr[a] = (int)(Math.random() * 100) + 1;
+        System.out.println("\nMerge Sort");
+        System.out.println("Array before sort:");
+        printArray(arr);
+        System.out.println();
+        //mergeSort(arr);
+        System.out.println("Array after sort:");
+        printArray(arr);
+        System.out.println();
 
     }
 
 }
-
-
