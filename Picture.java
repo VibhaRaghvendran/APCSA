1bebd55eb74bc3cd4a175a6506c9e174bbd74843
VibhaRaghvendran <vibha.shyama@gmail.com>
2025-02-14 14:54:15 -0800

    Update Picture.java

--- Picture.java
+++ Picture.java
@@ -106,6 +106,7 @@ public class Picture extends SimplePicture
     {
       for (Pixel pixelObj : rowArray)
       {
+		// sets red and green values to zero to only keep blue
         pixelObj.setRed(0);
         pixelObj.setGreen(0);
       }
@@ -120,6 +121,7 @@ public class Picture extends SimplePicture
     {
       for (Pixel pixelObj : rowArray)
       {
+		// sets everything to 255 - color to invert the colors
         pixelObj.setRed(255-pixelObj.getRed());
         pixelObj.setGreen(255-pixelObj.getGreen());
         pixelObj.setBlue(255-pixelObj.getBlue());
@@ -135,6 +137,8 @@ public class Picture extends SimplePicture
     {
       for (Pixel pixelObj : rowArray)
       {
+		// gray requires all RGB values to be the same - averages out
+		// current values and sets each color to the average
         int avg = (pixelObj.getRed() + pixelObj.getGreen() + pixelObj.getBlue())/3;
         pixelObj.setRed(avg);
         pixelObj.setGreen(avg);
@@ -201,84 +205,111 @@ public class Picture extends SimplePicture
 	*/
   public void pixelate(int size) {
 	  Pixel[][] pixels = this.getPixels2D();
-	  int maxX = pixels.length/size;
-	  int maxY = pixels[0].length/size;
-	  int sumRed = 0;
-	  int sumGreen = 0;
-	  int sumBlue = 0;
-	  for (int i = 0; i < (maxX-1)*size; i+=size) {
-		  for (int j = 0; j < (maxY-1)*size; j+=size) { 
-			  for (int k = i; k < i+size; k++) {
-				  for (int l = j; l < j+size; l++) {
-					  sumRed += pixels[k][l].getRed();
-					  sumGreen += pixels[k][l].getGreen();
-					  sumBlue += pixels[k][l].getBlue(); 
-				  }
-			  }
-			  
-			  int red = sumRed/(size*size);
-			  int green = sumGreen/(size*size);
-			  int blue = sumBlue/(size*size);
-			  
-			  for (int k = i; k < i+size; k++) {
-				  for (int l = j; l < j+size; l++) {
-					  pixels[k][l].setRed(red); 
-					  pixels[k][l].setGreen(green);
-					  pixels[k][l].setBlue(blue);
-				  }
-			  }
-			  sumRed = 0;
-		      sumGreen = 0;
-	          sumBlue = 0;
-		  }
-	  }
 	  
-	  if (maxX*size < pixels.length) {
-		  for (int j = 0; j < pixels[0].length; j+=size) {
-			  for (int k = maxY*size; k < pixels.length; k++) {
-				  for (int l = j; l < j+size; l++) {
-					  sumRed += pixels[k][l].getRed();
-					  sumGreen += pixels[k][l].getGreen();
-					  sumBlue += pixels[k][l].getBlue(); 
+	  // goes through whole array
+	  for (int i = 0; i < this.getHeight(); i += size) {
+		  for (int j = 0; j < this.getWidth(); j += size) {
+			  int sumRed = 0;
+			  int sumGreen = 0;
+			  int sumBlue = 0;
+			  
+			  // edge case # 1 - at the bottom, bottom most section doesn't 
+			  // divide out but column index still remaining
+			  if (i + size >= this.getHeight() && j + size < this.getWidth()) {
+				  int count = 0; // keeps track of how many pixels are in the section
+				  // creates a smaller section
+				  // gets the sums
+				  for (int k = i; k < this.getHeight(); k++) {
+					  for (int l = j; l < j+size; l++) {
+						  count++;
+						  Pixel obj = pixels[k][l];
+						  sumRed += obj.getRed();
+						  sumGreen += obj.getGreen();
+						  sumBlue += obj.getBlue();
+					  }
+				  }
+				  
+				  // puts the average values into each pixel in section
+				  for (int k = i; k < this.getHeight(); k++) {
+					  for (int l = j; l < j+size; l++) {
+						 pixels[k][l].setRed(sumRed/count);
+						 pixels[k][l].setGreen(sumGreen/count);
+						 pixels[k][l].setBlue(sumBlue/count);
+					  }
+				  }
+			  } 
+			  // edge case # 2 - at the rightmost section, section doesn't 
+			  // divide out but row index still remaining
+			  else if (i + size < this.getHeight() && j + size >= this.getWidth()) {
+				  int count = 0; // keeps track of how many pixels are in the section
+				  // creates a smaller section
+				  // gets the sums
+				  for (int k = i; k < i+size; k++) {
+					  for (int l = j; l < this.getWidth(); l++) {
+						  count++;
+						  Pixel obj = pixels[k][l];
+						  sumRed += obj.getRed();
+						  sumGreen += obj.getGreen();
+						  sumBlue += obj.getBlue();
+					  }
+				  }
+				  
+				  // puts the average values into each pixel in section
+				  for (int k = i; k < i+size; k++) {
+					  for (int l = j; l < this.getWidth(); l++) {
+						 pixels[k][l].setRed(sumRed/count);
+						 pixels[k][l].setGreen(sumGreen/count);
+						 pixels[k][l].setBlue(sumBlue/count);
+					  }
 				  }
 			  }
-			  
-			  for (int k = maxX*size; k < pixels.length; k++) {
-				  for (int l = j; l < j+size; l++) {
-					  pixels[k][l].setRed((sumRed)/(size^2)); 
-					  pixels[k][l].setGreen((sumGreen)/(size^2));
-					  pixels[k][l].setBlue((sumBlue)/(size^2));
+			  // edge case # 3 - at the bottom right corner, both don't divide out
+			  else if (i + size >= this.getHeight() && j + size >= this.getWidth()) {
+				  int count = 0; // keeps track of how many pixels are in the section
+				  // creates a smaller section
+				  // gets the sums
+				  for (int k = i; k < this.getHeight(); k++) {
+					  for (int l = j; l < this.getWidth(); l++) {
+						  count++;
+						  Pixel obj = pixels[k][l];
+						  sumRed += obj.getRed();
+						  sumGreen += obj.getGreen();
+						  sumBlue += obj.getBlue();
+					  }
+				  }
+				  
+				  // puts the average values into each pixel in section
+				  for (int k = i; k < this.getHeight(); k++) {
+					  for (int l = j; l < this.getWidth(); l++) {
+						 pixels[k][l].setRed(sumRed/count);
+						 pixels[k][l].setGreen(sumGreen/count);
+						 pixels[k][l].setBlue(sumBlue/count);
+					  }
 				  }
 			  }
-			  
-			  sumRed = 0;
-		      sumGreen = 0;
-	          sumBlue = 0;
-		  }
-	  }
-	  
-	  if (maxY*size < pixels[0].length) {
-		  for (int j = 0; j < pixels.length; j+=size) {
-			
-			  for (int k = maxX*size; k < pixels[0].length; k++) {
-				  for (int l = j; l < j+size; l++) {
-					  sumRed += pixels[k][l].getRed();
-					  sumGreen += pixels[k][l].getGreen();
-					  sumBlue += pixels[k][l].getBlue(); 
+			  // normal case
+			  else {
+				for (int k = 0; k < size; k++) {
+				
+				  // gets the sums
+				  for (int l = 0; l < size; l++) {
+					  Pixel obj = pixels[i+k][j+l];
+					  sumRed += obj.getRed();
+					  sumGreen += obj.getGreen();
+					  sumBlue += obj.getBlue();
 				  }
-			  }
+				}
 			  
-			  for (int k = maxY*size; k < pixels[0].length; k++) {
-				  for (int l = j; l < j+size; l++) {
-					  pixels[k][l].setRed((sumRed)/(size^2)); 
-					  pixels[k][l].setGreen((sumGreen)/(size^2));
-					  pixels[k][l].setBlue((sumBlue)/(size^2));
+			    // puts the average values into each pixel in section
+				for (int k = 0; k < size; k++) {
+				  for (int l = 0; l < size; l++) {
+					  Pixel obj = pixels[i+k][j+l];
+					  obj.setRed(sumRed/(size*size));
+					  obj.setGreen(sumGreen/(size*size));
+					  obj.setBlue(sumBlue/(size*size));
 				  }
-			  }
-			  
-			  sumRed = 0;
-		      sumGreen = 0;
-	          sumBlue = 0;
+				}
+			}
 		  }
 	  }
   }
@@ -287,12 +318,43 @@ public class Picture extends SimplePicture
 	* @param size Blur size, greater is more blur
 	* @return Blurred picture
 	*/
-  public void blur(int size)
+  public Picture blur(int size)
   {
 	Pixel[][] pixels = this.getPixels2D();
 	Picture result = new Picture(pixels.length, pixels[0].length);
 	Pixel[][] resultPixels = result.getPixels2D();
 	
+	// goes through all pixels
+	for (int i = 0; i < this.getHeight(); i++) {
+		for (int j = 0; j < this.getWidth(); j++) {
+			// sums
+			int sumRed = 0;
+			int sumGreen = 0;
+			int sumBlue = 0;
+			int count = 0; // keeps track of how many pixels are in the section 
+			
+			// creates a small square with selected pixel in the center
+			for (int k = i-(size/2); k < i+(size/2); k++) {
+				for (int l = j-(size/2); l < j+(size/2); l++) {
+					if (k >= 0 && k < this.getHeight() && l >= 0 && l < this.getWidth()) {
+						count++; // increments counter
+						Pixel obj = pixels[k][l];
+						// gets the sums
+						sumRed += obj.getRed();
+						sumGreen += obj.getGreen();
+						sumBlue += obj.getBlue();
+					}
+				}
+			}
+			
+			// puts in the average value into the center pixel of new image
+			resultPixels[i][j].setRed(sumRed/count);
+			resultPixels[i][j].setGreen(sumGreen/count);
+			resultPixels[i][j].setBlue(sumBlue/count);
+		}
+	}
+	
+	return result;
   } 
   
   /** Method that enhances a picture by getting average Color around
@@ -306,13 +368,149 @@ public class Picture extends SimplePicture
 	* and longer compute time.
 	* @return enhanced picture
 	*/
-   public void enhance(int size)
+   public Picture enhance(int size)
    {
      Pixel[][] pixels = this.getPixels2D();
      Picture result = new Picture(pixels.length, pixels[0].length);
      Pixel[][] resultPixels = result.getPixels2D();
      
+     // goes through all pixels
+     for (int i = 0; i < this.getHeight(); i++) {
+		for (int j = 0; j < this.getWidth(); j++) {
+			// sums
+			int sumRed = 0;
+			int sumGreen = 0;
+			int sumBlue = 0;
+			int count = 0; // keeps track of how many pixels are in the section 
+			
+			for (int k = i-(size/2); k < i+(size/2); k++) {
+				for (int l = j-(size/2); l < j+(size/2); l++) {
+					if (k >= 0 && k < this.getHeight() && l >= 0 && l < this.getWidth()) {
+						count++; // increments counter
+						Pixel obj = pixels[k][l];
+						// gets the sums
+						sumRed += obj.getRed();
+						sumGreen += obj.getGreen();
+						sumBlue += obj.getBlue();
+					}
+				}
+			}
+			
+			// puts in enhance characteristics into new image using average
+			resultPixels[i][j].setRed(2*pixels[i][j].getRed() - sumRed/count);
+			resultPixels[i][j].setBlue(2*pixels[i][j].getBlue() - sumBlue/count);
+			resultPixels[i][j].setGreen(2*pixels[i][j].getGreen() - sumGreen/count);
+		}
+	}
+	
+	return result;
    }
+  /** Swaps the left and right side of an image
+   *  @return The modified picture with swapped sides
+   */ 
+  public Picture swapLeftRight() {
+	 Pixel[][] pixels = this.getPixels2D();
+     Picture result = new Picture(pixels.length, pixels[0].length);
+     Pixel[][] resultPixels = result.getPixels2D();
+     
+     // goes through all the pixels
+     for (int i = 0; i < this.getHeight(); i++) {
+		for (int j = 0; j < this.getWidth(); j++) {
+			// computes new column number and puts in the old pixel into that column
+			Pixel resultPixel = resultPixels[i][(j+this.getWidth()/2) % this.getWidth()];
+			resultPixel.setColor(pixels[i][j].getColor());
+		}
+	 }
+	 
+	 // returns modified picture
+	 return result;
+  }
+  
+  /** Creates a jagged picture by shifting pixels a number of columns to the
+    * right based on the row number and the width of the "step"
+    * 
+	* @param shiftCount The number of pixels to shift to the right
+	* @param steps The number of steps
+	* @return The picture with pixels shifted in stair steps
+	*/
+  public Picture stairStep(int shiftCount, int steps) {
+	 Pixel[][] pixels = this.getPixels2D();
+     Picture result = new Picture(pixels.length, pixels[0].length);
+     Pixel[][] resultPixels = result.getPixels2D();
+     
+     // goes through all the pixels
+     for (int i = 0; i < this.getHeight(); i++) {
+		 // computes the right shift
+		 int shift = (i/(this.getHeight()/steps))*shiftCount;
+		 for (int j = 0; j < this.getWidth(); j++) {
+			 // computes the new column based on right shift, uses mod to cap overflow
+			 int newColumn = (j+shift)%this.getWidth();
+			 // puts in the old pixel into new column
+			 resultPixels[i][newColumn].setColor(pixels[i][j].getColor());
+		 }
+	 }
+	 
+	 // returns modified picture
+	 return result;
+  }
+  
+  /** Distorts the horizontal center of an image by shifting pixels horizontally
+    * based on a Gaussian curve
+    * 
+	* @param maxFactor Max height (shift) of curve in pixels
+	* @return Liquified picture
+	*/
+  public Picture liquify(int maxHeight) {
+	 Pixel[][] pixels = this.getPixels2D();
+     Picture result = new Picture(pixels.length, pixels[0].length);
+     Pixel[][] resultPixels = result.getPixels2D();
+     
+     // sets the width of the bell in the bell curve
+     int bellWidth = 100;
+     // goes through all the pixels
+     for (int i = 0; i < this.getHeight(); i++) {
+		for (int j = 0; j < this.getWidth(); j++) {
+			// computes exponent
+			double exponent = Math.pow(i - this.getHeight()/2.0, 2)/(2.0*Math.pow(bellWidth, 2));
+			// computes right shift for each pixel
+			int rightShift = (int)(maxHeight * Math.exp(- exponent));
+			
+			// computes new column, caps overflow
+			int newColumn = (j+rightShift)%this.getWidth();
+			// puts old pixel into new column
+			resultPixels[i][newColumn].setColor(pixels[i][j].getColor());
+		}
+	 }
+	 
+	 // returns modified picture
+	 return result;
+  }
+  
+  /** Creates oscillating distortions in a picture based on a sine curve
+	* @param amplitude The maximum shift of pixels
+	* @return Wavy picture
+	*/
+  public Picture wavy(int amplitude) {
+	 Pixel[][] pixels = this.getPixels2D();
+     Picture result = new Picture(pixels.length, pixels[0].length);
+     Pixel[][] resultPixels = result.getPixels2D();
+     
+     double frequency = 0.01; // can change amplitude
+     // goes through all pixels
+     for (int i = 0; i < this.getHeight(); i++) {
+		// computes shift for each row
+		int shift = (int)(amplitude * Math.sin(2*Math.PI*frequency*i));
+		for (int j = 0; j < this.getWidth(); j++) {
+			// computes new column
+			int newColumn = ((((j+shift)%this.getWidth())+this.getWidth())%this.getWidth());
+			// puts old pixel into new column
+			resultPixels[i][newColumn].setColor(pixels[i][j].getColor());
+		}
+	 }
+	 
+	 // returns modified picture
+	 return result;
+  }
   
   /** Method that mirrors the picture around a 
     * vertical mirror in the center of the picture
@@ -433,6 +631,57 @@ public class Picture extends SimplePicture
     }
   }
   
+  /** Method that creates an edge detected black/white picture
+	* @param threshold threshold as determined by Pixel’s colorDistance method
+	* @return edge detected picture
+	*/
+  public Picture edgeDetectionBelow(int edgeDist)
+  {
+    Pixel upPixel = null;
+    Pixel downPixel = null;
+    Pixel[][] pixels = this.getPixels2D();
+    Picture result = new Picture(pixels.length, pixels[0].length);
+	Pixel[][] resultPixels = result.getPixels2D(); 
+    Color downColor = null;
+    
+    for (int col = 0; col < pixels[0].length; col++)
+    {
+      for (int row = 0; 
+           row < pixels.length-1; row++)
+      {
+        upPixel = pixels[row][col];
+        downPixel = pixels[row+1][col];
+        downColor = downPixel.getColor();
+        if (upPixel.colorDistance(downColor) > 
+            edgeDist)
+          resultPixels[row][col].setColor(Color.BLACK);
+        else
+          resultPixels[row][col].setColor(Color.WHITE);
+      }
+    }
+    
+    return result;
+  }
+  
+  /** Method that creates a green screen picture
+	* @return green screen picture
+	*/
+  public Picture greenScreen()
+  {
+	// Get background picture
+	Picture bkgnd = new Picture("greenScreenImages/IndoorHouseLibraryBackground.jpg");
+	Pixel[][] bkgndPixels = bkgnd.getPixels2D();
+	// Get minion1 picture
+	Picture minion1 = new Picture("greenScreenImages/minion1GreenScreen.jpg");
+	Pixel[][] minion1Pixels = minion1.getPixels2D();
+	minion1 = minion1.scale(0.7, 0.7);
+	// Get minion2 picture
+	Picture minion2 = new Picture("greenScreenImages/minion2GreenScreen.jpg");
+	Pixel[][] minion2Pixels = minion2.getPixels2D(); 
+	minion2 = minion2.scale(0.4, 0.4);
+	
+	return bkgnd;
+  }
   
   /* Main method for testing - each class in Java can have a main 
    * method 
